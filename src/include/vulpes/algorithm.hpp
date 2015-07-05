20a2a95523e421f27c428c0d56915378ad46570e
Aditya Avinash Atluri <adityaavinash1@gmail.com>
2015-07-05 06:56:18 +0530

    Create algorithm.hpp

---
+++ src/include/vulpes/algorithm.hpp
@@ -0,0 +1,21 @@
+//
+//  algorithm.hpp
+//
+//  Created by Aditya Atluri on 7/3/15.
+//  Copyright © 2015 Aditya Atluri. All rights reserved.
+//
+
+#ifndef algorithm_hpp
+#define algorithm_hpp
+
+#include "device.hpp"
+
+namespace vulpes{
+    
+#define METAL_SRC(src) "#include<metal_stdlib>\n using namespace metal;\n"#src
+    
+    class algorithm{
+    };
+}
+
+#endif /* algorithm_hpp */
