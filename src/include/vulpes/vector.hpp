1d65fe5506ec6d4059adc2e7f6c0dbe1ee7221ea
Aditya Avinash Atluri <adityaavinash1@gmail.com>
2015-07-12 10:52:21 +0530

    Added feature for size, and assign operator overloading
    Now, vector length is returned.
    A device vector can be assigned to another device vector.

--- src/include/vulpes/vector.hpp
+++ src/include/vulpes/vector.hpp
@@ -19,7 +19,7 @@ namespace vulpes{
     private:
         size_t len;
         device _device;
-        size_t size;
+        size_t Size;
         id<MTLBuffer> buffer;
         T *hostptr;
     public:
@@ -27,61 +27,61 @@ namespace vulpes{
         vector(size_t _len, device __device){
             len = _len;
             _device = __device;
-            size = sizeof(T)*len;
+            Size = sizeof(T)*len;
             hostptr = NULL;
         }
         vector(std::vector<T> &host, device __device){
             len = host.size();
-            size = sizeof(T) * len;
+            Size = sizeof(T) * len;
             hostptr = &host[0];
             _device = __device;
-            buffer = [__device.get_device() newBufferWithBytes:&host[0] length:size options:0];
+            buffer = [__device.get_device() newBufferWithBytes:&host[0] length:Size options:0];
         }
         vector(std::vector<T> &host){
             len = host.size();
-            size = sizeof(T) * len;
+            Size = sizeof(T) * len;
             hostptr = &host[0];
-            buffer = [_device.get_device() newBufferWithBytes:&host[0] length:size options:0];
+            buffer = [_device.get_device() newBufferWithBytes:&host[0] length:Size options:0];
         }
         vector(std::vector<T,aligned_allocator<T>> &host){
             len = host.size();
-            size = sizeof(T) * len;
-            if(size%4096 > 0){
-                size = ((size/4096)+1)*4096;
+            Size = sizeof(T) * len;
+            if(Size%4096 > 0){
+                Size = ((Size/4096)+1)*4096;
             }
             hostptr = &host[0];
-            buffer = [_device.get_device() newBufferWithBytesNoCopy:&host[0] length:size options:0 deallocator:nil];
+            buffer = [_device.get_device() newBufferWithBytesNoCopy:&host[0] length:Size options:0 deallocator:nil];
         }
         vector(std::vector<T,aligned_allocator<T>> &host, device __device){
             len = host.size();
             _device = __device;
-            size = sizeof(T) * len;
-            if(size%4096 > 0){
-                size = ((size/4096)+1)*4096;
+            Size = sizeof(T) * len;
+            if(Size%4096 > 0){
+                Size = ((Size/4096)+1)*4096;
             }
             hostptr = &host[0];
-            buffer = [__device.get_device() newBufferWithBytesNoCopy:&host[0] length:size options:0 deallocator:nil];
+            buffer = [__device.get_device() newBufferWithBytesNoCopy:&host[0] length:Size options:0 deallocator:nil];
         }
         vector(T* start, T* end){
             hostptr = start;
             len = end - start;
-            size = sizeof(T)*len;
-            buffer = [_device.get_device() newBufferWithBytes:start length:size options:0];
+            Size = sizeof(T)*len;
+            buffer = [_device.get_device() newBufferWithBytes:start length:Size options:0];
         }
         vector(size_t _len, T value){
             len = _len;
-            size = sizeof(T)*len;
+            Size = sizeof(T)*len;
             std::vector<T> host(_len, value);
             hostptr = &host[0];
-            buffer = [_device.get_device() newBufferWithBytes:&host[0] length:size options:0];
+            buffer = [_device.get_device() newBufferWithBytes:&host[0] length:Size options:0];
         }
         Iterator<T> begin(){
-            vulpes::Iterator<T> _iterator(_device, &buffer, hostptr, sizeof(T), size);
+            vulpes::Iterator<T> _iterator(_device, &buffer, hostptr, sizeof(T), Size);
             return _iterator;
         }
         
         Iterator<T> end(){
-            vulpes::Iterator<T> _iterator(_device, &buffer, (T*)hostptr+len, sizeof(T), size);
+            vulpes::Iterator<T> _iterator(_device, &buffer, (T*)hostptr+len, sizeof(T), Size);
             return _iterator;
         }
         id<MTLBuffer> get_buffer(){
@@ -98,8 +98,14 @@ namespace vulpes{
             vector<T> lhs(rhs);
             return lhs;
         }
+        vulpes::vector<T>& operator=(vulpes::vector<T>& rhs){
+            return rhs;
+        }
         T& operator[](int id){
             return *(hostptr+id);
+        }
+        size_t size(){
+            return len;
         }
     };
 }
