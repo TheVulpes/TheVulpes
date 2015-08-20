a0f6c031364f301e94082da9190e0d981c32a1c4
Aditya Avinash Atluri <adityaavinash1@gmail.com>
2015-08-20 10:04:57 -0400

    Included algorithm header files

---
+++ src/include/vulpes/algorithm.hpp
@@ -0,0 +1,22 @@
+//
+//  algorithm.hpp
+//
+//  Created by Aditya Atluri on 8/20/15.
+//  Copyright © 2015 Aditya Atluri. All rights reserved.
+//
+
+#ifndef algorithm_hpp
+#define algorithm_hpp
+
+#include "device.hpp"
+#include "functional.hpp"
+#include "program.hpp"
+#include "./container/vector.hpp"
+#include "./algorithm/fill.hpp"
+#include "./algorithm/fill_n.hpp"
+#include "./algorithm/replace.hpp"
+#include "./algorithm/transform.hpp"
+#include "./algorithm/sequence.hpp"
+
+
+#endif /* algorithm_hpp */
